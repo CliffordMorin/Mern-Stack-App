60ce0242410a2fb916a90ac07c9b382bb60e8be2
Clifford Morin <cemorin21@gmail.com>
2022-02-10 14:34:29 -0500

    connected server to database

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit:"32mb", extend: true}))
+app.use(bodyParser.urlencoded({limit:"32mb", extend: true}))
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://cliff:tq554Gs4hvCIHp2e@cluster0.brylt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5001;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
+  .catch(err => console.log(err.message))
