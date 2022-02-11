00e6f10cf493726e5d5ef8c90c1ebf0e12e00cb2
Clifford Morin <cemorin21@gmail.com>
2022-02-11 00:08:55 -0500

    added all controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,27 @@
-export const getPosts =  (req, res) => {
-    res.send('Post Works!')
+import PostMessage from "../models/postMessage.js"
+
+export const getPosts = async (req, res) => {
+    try{
+        const postMessages = await PostMessage.find()
+        console.log(postMessages)
+
+        res.status(200).json(postMessages)
     }
+    catch (error){
+        res.status(404).json({message: error.message})
+    }
+    }
+
+export const createPost = async (req, res) => {
+    const body = req.body;
+    
+    const newPost = new PostMessage(body);
+
+    try {
+        await newPost.save()
+
+        re.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
+}
