0e239b90f3903c930e48d97bc68ebe63a3d572ef
Clifford Morin <cemorin21@gmail.com>
2022-02-10 23:58:50 -0500

    added model, controllers and routes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
+
 const app = express();
 
 app.use(bodyParser.json({limit:"32mb", extend: true}))
 app.use(bodyParser.urlencoded({limit:"32mb", extend: true}))
 app.use(cors())
+
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://cliff:tq554Gs4hvCIHp2e@cluster0.brylt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5001;
