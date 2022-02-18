34f3dbdc111ca30e99827813478ef3c9baaa6a38
Clifford Morin <cemorin21@gmail.com>
2022-02-18 02:15:50 -0500

    added form for auth adn functionalitiy for the sign up / sign in form

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
-    console.log(postMessages);
 
     res.status(200).json(postMessages);
   } catch (error) {
