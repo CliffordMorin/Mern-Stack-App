48ce0c3fb2068a24e2f0282b589e1a9790aa1e7f
Clifford Morin <cemorin21@gmail.com>
2022-02-22 23:42:45 -0500

    fixed post deleting from page from reducers

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -9,7 +9,7 @@ import {
 const reducer = (posts = [], action) => {
   switch (action.type) {
     case DELETE:
-      return posts.filter((post) => posts._id !== action.payload);
+      return posts.filter((post) => post._id !== action.payload);
     case UPDATE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
